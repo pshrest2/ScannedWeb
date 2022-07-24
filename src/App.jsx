1ba112571f8d943de7d1c17f146fd47271940d18
pshrestha <pshrest2@go.olemiss.edu>
2022-07-24 15:14:21 -0500

    added a signin page with a simple form validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import './App.scss';
+import Signin from './Pages/Signin/Signin';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Signin />} />
     </Routes>
   );
 }
